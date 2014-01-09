13d50164abb63074567ea7467342a3f5b9b9bbb6
Yosuke Kumakura <kumatch@gmail.com>
2014-01-09 15:32:17 +0900

    fixed the failed to chain replacing if changes variable rule.

--- test/breath.variable_rule.test.js
+++ test/breath.variable_rule.test.js
@@ -5,7 +5,7 @@ var breath = require('../lib/breath');
 
 describe('setTemplateVariableRule()', function () {
     var core = {
-        foo: 'foo {{= a }} and <%= a %>',
+        foo: 'foo {{=a}} and <%= a %>',
 
         bar: {
             baz: {
@@ -24,10 +24,16 @@ describe('setTemplateVariableRule()', function () {
         var origin  = breath(core).createSync(addition);
         var changes = breath(core).setTemplateVariableRule('{{', '}}').createSync(addition);
 
-        expect(origin.foo).be.equal('foo {{= a }} and OK');
+        expect(origin.foo).be.equal('foo {{=a}} and OK');
         expect(origin.bar.baz.qux[0]).be.equal('bar baz qux {{= b.c[1] }} and bbb');
 
         expect(changes.foo).be.equal('foo OK and <%= a %>');
         expect(changes.bar.baz.qux[0]).be.equal('bar baz qux bbb and <%= b.c[1] %>');
+    });
+
+    it('should success replacing if changes rule with replacing chain', function () {
+        var changes = breath({ foo: "foo with {{=a}}"}).setTemplateVariableRule('{{', '}}').createSync({ a: "a and {{= b }}", b: "b and {{= c }}", c: "c" });
+
+        expect(changes.foo).be.equal('foo with a and b and c');
     });
 });
